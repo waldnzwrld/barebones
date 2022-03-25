3dc887b65f11eaf0d5ac9f814634724d52123dd0
Walden Bodtker <waldnzwrld@gmail.com>
2022-03-24 21:53:16 -0700

    use bodyparser

--- server/server.js
+++ server/server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 const apiRouter = require('./routes/apiRouter');
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
 app.use ('/api', apiRouter)
 
 app.listen(port, () => {
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,32 +1,28 @@
 import '../App.css';
 import axios from 'axios';
-import React from 'react';
+import { useState, useEffect } from 'react';
 
-class Home extends React.Component {
-  state = {
-    appName: ''
-  }
+export default () => {
+  const [appName, setAppName] = useState('');
+
+  useEffect(() => {
+    getAppName()
+  }, []);
 
   // DB has a plain ol js object named "BareBones" with an id of 1
-  getAppName = () => { axios.get('/api/pojso/1').then(res => this.setAppName((res.data))) }
-  setAppName = (name) => { this.setState({ appName: name }) }
-
-  componentDidMount() {
-    this.getAppName()
+  function getAppName() {
+    axios.get('/api/pojso/1')
+      .then(res => setAppName((res.data)))
+      .catch(err => console.error(err));
   }
 
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            {this.state.appName}
-          </p>
-        </header>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          {appName}
+        </p>
+      </header>
+    </div>
+  );
 }
-
-export default Home;
