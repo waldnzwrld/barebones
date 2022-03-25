d61facae88a65ed755fc6f70543adca8200a7e20
Walden Bodtker <waldnzwrld@gmail.com>
2022-03-25 11:03:11 -0700

    improve dbseed

--- script/seeddb.js
+++ script/seeddb.js
@@ -3,13 +3,14 @@
 const Pojso = require('../server/models/pojso');
 
 Pojso.where({id: 1}).fetch().then(function(pojso) {
-        if (!pojso) {
-            new Pojso({name: "BareBones"}).save().then(function(model) {
-                console.log(`seeded db with ${model.toJSON()}`)
-                process.exit(0)
-            });
-        } else {
-            console.log(`db already seeded with ${pojso.get('name')}`);
-            process.exit(0)
-        }
+        console.log(`db already seeded with ${pojso.get('name')}`);
+        process.exit(0)
+    }).catch(function() {
+        new Pojso({name: "BareBones"}).save().then(function(pojso) {
+          console.log(`seeded db with Plain Ol JSON Object: {id: ${pojso.get('id')}, name: ${pojso.get('name')}`);
+          process.exit(0)
+        }).catch(function(err) {
+          console.log("error seeding db", err.message);
+          process.exit(1)
+        })
     });
