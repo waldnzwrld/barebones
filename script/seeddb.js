c0156f23ad976944356c9ed90606152b165cf456
Walden Bodtker <waldnzwrld@gmail.com>
2022-03-25 11:06:59 -0700

    bracket in comment

--- script/seeddb.js
+++ script/seeddb.js
@@ -7,7 +7,7 @@ Pojso.where({id: 1}).fetch().then(function(pojso) {
         process.exit(0)
     }).catch(function() {
         new Pojso({name: "BareBones"}).save().then(function(pojso) {
-          console.log(`seeded db with Plain Ol JSON Object: {id: ${pojso.get('id')}, name: ${pojso.get('name')}`);
+          console.log(`seeded db with Plain Ol JSON Object: {id: ${pojso.get('id')}, name: ${pojso.get('name')}}`);
           process.exit(0)
         }).catch(function(err) {
           console.log("error seeding db", err.message);
